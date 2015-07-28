f264988698dbd013567a3bb140d823924f3a83a8
Paweł Chojnowski <pawel.chojnowski@live.com>
2015-07-28 16:11:30 +0200

    Added global handling of log level

--- src/main/java/pl/nask/hsn2/os/Main.java
+++ src/main/java/pl/nask/hsn2/os/Main.java
@@ -40,78 +40,84 @@ import org.apache.commons.daemon.DaemonInitException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import pl.nask.hsn2.logger.LoggerForLog4j;
+import pl.nask.hsn2.logger.LoggerManager;
+
 public class Main implements Daemon {
 	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+	private static LoggerManager loggerManager = LoggerForLog4j.getInstance();
 	private static String connectorAddress = "127.0.0.1";
-    private static String mongoAddress = "127.0.0.1";
-    private static String objectStoreQueueNameLow = "os:l";
-    private static String objectStoreQueueNameHigh = "os:h";
-    private static int maxListenerLpThreads = 5;
-    private static int maxListenerHpThreads = 5;
-    private static ExecutorService executor;
+	private static String mongoAddress = "127.0.0.1";
+	private static String objectStoreQueueNameLow = "os:l";
+	private static String objectStoreQueueNameHigh = "os:h";
+	private static int maxListenerLpThreads = 5;
+	private static int maxListenerHpThreads = 5;
+	private static ExecutorService executor;
 	private static String jobFinishedIgnore = "none";
 
-    public static void main(String[] args) throws DaemonInitException, Exception {
-    	Main os = new Main();
-    	os.init(new JsvcArgWrapper(args));
-    	os.start();
-    	Thread.currentThread().join();
-    	os.stop();
-    	os.destroy();
-    }
+	public static void main(String[] args) throws DaemonInitException, Exception {
+		Main os = new Main();
+		os.init(new JsvcArgWrapper(args));
+		os.start();
+		Thread.currentThread().join();
+		os.stop();
+		os.destroy();
+	}
 
-    private static void createConnectors(List<Callable<Object>> connectors, int count, String queueName){
-    	for (int i=0; i < count; i++) {
-            ObjectStoreConnector connector = new ObjectStoreConnector(queueName, jobFinishedIgnore);
-            connectors.add(Executors.callable(connector));
-        }
-    }
-    
-    @SuppressWarnings("static-access")
-    private static CommandLine parseArguments(String[] args) {
-        Options options = new Options();
-        options.addOption(OptionBuilder.withLongOpt("mongoConnector").withArgName("address").hasArg().withDescription("Mongo connector address").create("mongoCon"));
-        options.addOption(OptionBuilder.withLongOpt("connector").withArgName("address").hasArg().withDescription("Connector address").create("con"));
-        options.addOption(OptionBuilder.withLongOpt("queueName").withArgName("name").hasArg().withDescription("ObjectStore low priority queue name").create("qn"));
-        options.addOption(OptionBuilder.withLongOpt("highQueueName").withArgName("name").hasArg().withDescription("ObjectStore high priority queue name").create("hqn"));
-        options.addOption(OptionBuilder.withLongOpt("maxListenerThreads").withArgName("num").hasArg().withDescription("listener threads pool size for low priority queue").create("maxLT"));
-        options.addOption(OptionBuilder.withLongOpt("help").withDescription("prints help").create("h"));
-        options.addOption(OptionBuilder.withLongOpt("jobFinishedIgnore").withArgName("all|fail|none").hasArg().withDescription("Ignore job finished. default: none").create("jfi"));
-        try {
-            CommandLine p = new PosixParser().parse(options,args);
-            if (p.hasOption("help")) {
-                HelpFormatter f = new HelpFormatter();
-                f.printHelp("java -jar ...", options);
-                System.exit(0);
-            }
-            return p;
-        } catch (ParseException e) {
-            throw new IllegalArgumentException("Cannot parse command line options", e);
-        }
-    }
+	private static void createConnectors(List<Callable<Object>> connectors, int count, String queueName) {
+		for (int i = 0; i < count; i++) {
+			ObjectStoreConnector connector = new ObjectStoreConnector(queueName, jobFinishedIgnore);
+			connectors.add(Executors.callable(connector));
+		}
+	}
 
-    private static void applyArguments(CommandLine cmd) {
-        if (cmd.hasOption("connector"))
-            connectorAddress = cmd.getOptionValue("connector");
-        if (cmd.hasOption("mongoConnector"))
-        	mongoAddress = cmd.getOptionValue("mongoConnector");
-        if (cmd.hasOption("queueName"))
-            objectStoreQueueNameLow = cmd.getOptionValue("queueName");
-        if (cmd.hasOption("highQueueName"))
-            objectStoreQueueNameHigh = cmd.getOptionValue("highQueueName");
-        if (cmd.hasOption("maxListenerThreads"))
-            maxListenerLpThreads = Integer.parseInt(cmd.getOptionValue("maxListenerThreads"));
-        if (cmd.hasOption("maxListenerHpThreads"))
-            maxListenerHpThreads = Integer.parseInt(cmd.getOptionValue("maxListenerHpThreads"));
-        if (cmd.hasOption("jobFinishedIgnore"))
-        	jobFinishedIgnore = cmd.getOptionValue("jobFinishedIgnore");
-    }
+	@SuppressWarnings("static-access")
+	private static CommandLine parseArguments(String[] args) {
+		Options options = new Options();
+		options.addOption(OptionBuilder.withLongOpt("logLevel").withArgName("level").hasArg().withDescription("use given level for log").create("ll"));
+		options.addOption(OptionBuilder.withLongOpt("mongoConnector").withArgName("address").hasArg().withDescription("Mongo connector address").create("mongoCon"));
+		options.addOption(OptionBuilder.withLongOpt("connector").withArgName("address").hasArg().withDescription("Connector address").create("con"));
+		options.addOption(OptionBuilder.withLongOpt("queueName").withArgName("name").hasArg().withDescription("ObjectStore low priority queue name").create("qn"));
+		options.addOption(OptionBuilder.withLongOpt("highQueueName").withArgName("name").hasArg().withDescription("ObjectStore high priority queue name").create("hqn"));
+		options.addOption(OptionBuilder.withLongOpt("maxListenerThreads").withArgName("num").hasArg().withDescription("listener threads pool size for low priority queue").create("maxLT"));
+		options.addOption(OptionBuilder.withLongOpt("help").withDescription("prints help").create("h"));
+		options.addOption(OptionBuilder.withLongOpt("jobFinishedIgnore").withArgName("all|fail|none").hasArg().withDescription("Ignore job finished. default: none").create("jfi"));
+		try {
+			CommandLine p = new PosixParser().parse(options, args);
+			if (p.hasOption("help")) {
+				HelpFormatter f = new HelpFormatter();
+				f.printHelp("java -jar ...", options);
+				System.exit(0);
+			}
+			return p;
+		} catch (ParseException e) {
+			throw new IllegalArgumentException("Cannot parse command line options", e);
+		}
+	}
+
+	private static void applyArguments(CommandLine cmd) {
+		if (cmd.hasOption("logLevel"))
+			loggerManager.setLogLevel(cmd.getOptionValue("logLevel"));
+		if (cmd.hasOption("connector"))
+			connectorAddress = cmd.getOptionValue("connector");
+		if (cmd.hasOption("mongoConnector"))
+			mongoAddress = cmd.getOptionValue("mongoConnector");
+		if (cmd.hasOption("queueName"))
+			objectStoreQueueNameLow = cmd.getOptionValue("queueName");
+		if (cmd.hasOption("highQueueName"))
+			objectStoreQueueNameHigh = cmd.getOptionValue("highQueueName");
+		if (cmd.hasOption("maxListenerThreads"))
+			maxListenerLpThreads = Integer.parseInt(cmd.getOptionValue("maxListenerThreads"));
+		if (cmd.hasOption("maxListenerHpThreads"))
+			maxListenerHpThreads = Integer.parseInt(cmd.getOptionValue("maxListenerHpThreads"));
+		if (cmd.hasOption("jobFinishedIgnore"))
+			jobFinishedIgnore = cmd.getOptionValue("jobFinishedIgnore");
+	}
 
 	@Override
-	public void init(DaemonContext context) throws DaemonInitException,	Exception {
+	public void init(DaemonContext context) throws DaemonInitException, Exception {
 		CommandLine cmd = parseArguments(context.getArguments());
 		applyArguments(cmd);
-
 	}
 
 	@Override
@@ -120,22 +126,22 @@ public class Main implements Daemon {
 		try {
 			MongoConnector.initConnection(mongoAddress, 27017);
 		} catch (IOException e) {
-			LOGGER.error("Problem with MongoDB!",e);
+			LOGGER.error("Problem with MongoDB!", e);
 			System.exit(1);
 		}
-		Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {		
+		Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {
 			@Override
 			public void uncaughtException(Thread t, Throwable e) {
-				System.exit(1);		
+				System.exit(1);
 			}
 		});
-		
+
 		executor = Executors.newFixedThreadPool(maxListenerLpThreads + maxListenerHpThreads);
 		List<Callable<Object>> connectors = new ArrayList<>();
 		createConnectors(connectors, maxListenerLpThreads, objectStoreQueueNameLow);
 		createConnectors(connectors, maxListenerHpThreads, objectStoreQueueNameHigh);
-		//        executor.invokeAll(connectors);
-		for (Callable<Object> t:connectors) {
+		// executor.invokeAll(connectors);
+		for (Callable<Object> t : connectors) {
 			executor.submit(t);
 		}
 
@@ -144,24 +150,22 @@ public class Main implements Daemon {
 	@Override
 	public void stop() throws Exception {
 		executor.shutdownNow();
-		
+
 	}
 
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
-		
 	}
-	
-	
-	
-	
+
 	private static class JsvcArgWrapper implements DaemonContext {
 
 		private final String[] args;
-		public JsvcArgWrapper(String [] p) {
+
+		public JsvcArgWrapper(String[] p) {
 			this.args = p;
 		}
+
 		@Override
 		public DaemonController getController() {
 			return null;
@@ -171,6 +175,6 @@ public class Main implements Daemon {
 		public String[] getArguments() {
 			return this.args;
 		}
-		
+
 	}
 }
